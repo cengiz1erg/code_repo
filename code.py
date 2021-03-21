24de2cc6807835dc5b1030e6c5fbe276d7ec273f
Cengiz <cengiz1ergun@gmail.com>
2021-03-21 12:51:31 +0300

    decorator kullanımının yan etkisi ve çözümü

--- code.py
+++ code.py
@@ -1,9 +1,16 @@
+from functools import wraps
+import inspect
+
+
 def counter(fn):
     """Bu fonksiyon bir 'decorator' olup çıkışa "inner" adında bir 'closure' iletir. İçerdeki inner fonksiyonu,
     kullanılacak asıl fonksiyonları modifiye eder. Burada modifiye olayı kullanılan fonksiyonun kaçıncı defa
     çağırıldığını ekrana basma'dır. """
     count = 0
+    # fonksiyon aynı ada sahip olsa da artık inner closure döndürüleceğinden debugging sorunu var. fn fonksiyonu yerine closure'ın
+    # 'metadata,docstring,signature vs'si döndürülecek. Çözüm:
 
+    @wraps(fn)
     def inner(*args, **kwargs):  # "positional" ve "keyword" değişkenleri paketle
         nonlocal count  # dış fonksiyondaki count etiketinin referansına ulaş
         count += 1
@@ -12,15 +19,14 @@ def counter(fn):
     return inner
 
 
+@counter
 def add(a, b):
     return a + b
 
 
+@counter
 def mult(a, b):
     return a * b
 
 
-#-----------
-add = counter(add)
-mult = counter(mult)
-#-----------
+print(inspect.signature(mult))  # çıktı: (a, b)
